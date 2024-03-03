29ac6fa49df15148992258bc819699115d1cd546
zehava-drabkin <z0583212849@gmail.com>
2024-03-03 12:14:33 +0200

    template to all users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import Users from './users.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'learnGit';
+  public users: Users[] = [
+    {
+      firstName: "Zehavi",
+      lastName: "Fainman",
+      isLikeAngular: true,
+    }
+  ];
 }
---
+++ src/app/users.model.ts
@@ -0,0 +1,5 @@
+export default class Users {
+    firstName!: string
+    lastName!: string
+    isLikeAngular!: boolean
+}
